08f80d928c76d41f0b6a78a68422572db29ba633
aishwarya234 <45032806+aishwarya234@users.noreply.github.com>
2019-12-27 14:52:28 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,34 @@
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+public class Account {
+
+
+		public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+		// TODO Auto-generated method stub
+		private int accountNumber;
+		private String customerName;
+		private double balance;
+		
+	}
+
+
+
+
+
